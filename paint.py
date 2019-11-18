436834439660e50933cdc672d624db8fc9f4f636
Vics <3717247+VicsEscamilla@users.noreply.github.com>
2019-11-18 17:08:57 -0600

    Adding paint

--- paint.py
+++ paint.py
@@ -4,7 +4,34 @@ import random
 
 from keras.models import load_model
 
+drawing=False # true if mouse is pressed
+mode=True
+frame = np.zeros((800,1200,3), np.uint8) + 255
+ 
+# mouse callback function
+def paint_draw(event,former_x,former_y,flags,param):
+    global current_former_x,current_former_y,drawing, mode, frame
+ 
+    if event==cv2.EVENT_LBUTTONDOWN:
+        drawing=True
+        current_former_x,current_former_y=former_x,former_y
+ 
+    elif event==cv2.EVENT_MOUSEMOVE:
+        if drawing==True:
+            if mode==True:
+                cv2.line(frame,(current_former_x,current_former_y),(former_x,former_y),(0, 0, 0),3)
+                current_former_x = former_x
+                current_former_y = former_y
+    elif event==cv2.EVENT_LBUTTONUP:
+        drawing=False
+        if mode==True:
+            cv2.line(frame,(current_former_x,current_former_y),(former_x,former_y),(0, 0, 0),3)
+            current_former_x = former_x
+            current_former_y = former_y
+    return former_x,former_y
+
 def start():
+    global frame
     BORDERS = (0, 255, 0)
     LABELS = (255, 0, 0)
     LIST = (255, 0, 0)
@@ -18,10 +45,12 @@ def start():
     ]
 
     model = load_model('working_model.h5')
-    cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(0)
 
+    cv2.namedWindow('frame')
+    cv2.setMouseCallback('frame',paint_draw)
+    last_predictions = {}
     while True:
-        _, frame = cap.read()
 
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
@@ -31,8 +60,7 @@ def start():
 
         rectangles = [(cv2.boundingRect(c), hierarchy) for c in contours]
 
-        guesses = ['banana', 'house', 'candle', 'fork', 'pizza']
-
+        predictions = {}
         for r, h in rectangles:
             if r[2] < 20 and r[3] < 20:
                 continue
@@ -52,21 +80,28 @@ def start():
             final = np.reshape(cv2.dilate(roi, (3, 3, 3)), (1, 1, 28, 28))
             prediction = model.predict_classes(final)[0]
             proba = model.predict_proba(final)[0]
-            if proba[prediction] > 0.70:
-                label = classes[prediction]
-                cv2.rectangle(frame, (r[0], r[1]), (r[0] + r[2], r[1] + r[3]), BORDERS, 2)
-                cv2.putText(frame, label, (r[0], r[1]),cv2.FONT_HERSHEY_DUPLEX, 1, LABELS, 2)
-                if label in guesses:
-                    cv2.imshow(label, roi)
+            # if proba[prediction] > 0.50:
+            try:
+                tmp = cv2.resize(roi, (200, 200), interpolation=cv2.INTER_AREA)
+                predictions[classes[prediction]] = tmp
+            except:
+                continue
 
-        # show camera input
+        for label in [x for x in last_predictions if x not in predictions]:
+            cv2.destroyWindow(label)
+
+        for label, roi in predictions.items():
+            cv2.imshow(label, roi)
+        last_predictions = predictions
+
         cv2.imshow('frame', frame)
-        cv2.imshow("threshold", im_th)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        key = cv2.waitKey(1)
+        if key & 0xFF == ord('q'):
             break
+        if key == 27: #Escape KEY
+            frame = np.zeros((800,1200,3), np.uint8) + 255
 
-    cap.release()
     cv2.destroyAllWindows()
 
 if __name__=="__main__":
